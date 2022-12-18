057ae0b68f438f8c9f4fb7e2211624e2819ee94c
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2022-12-18 12:10:40 +0000

    event listener for click event > search then save city

--- assets/js/modules/script-wip.js
+++ assets/js/modules/script-wip.js
@@ -4,9 +4,19 @@ const yourKey = x;
 
 var API_KEY = yourKey; // register for a free key at openweathermap.com
 
-// Grab HTML Elements
+// Assign HTML Elements to variables
 var searchBtn = document.querySelector("#search-button");
 var userInput = document.querySelector("#user-search-input");
 var recentSearch = document.querySelector("#recent-search");
+userInput.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    // Get user input: city
+    var cityInput = userInput.value.trim().toLowerCase();
+    city = toTitleCase(cityInput);
 
+    searchCity(city, API_KEY); // search city's current weather and display on html
+    getCityGeo(city, API_KEY); // to get the city's geo latitudes & longitudes
+    saveRecentSearch(city); // to save the recent search to local storage & display under the search box
+  }
+});
 
