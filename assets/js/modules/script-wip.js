defb438dadae76a0459fd7c71c3116f523783dbe
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2022-12-18 11:56:56 +0000

    assign html elements to variables

---
+++ assets/js/modules/script-wip.js
@@ -0,0 +1,12 @@
+import { x } from './config.js';
+
+const yourKey = x;
+
+var API_KEY = yourKey; // register for a free key at openweathermap.com
+
+// Grab HTML Elements
+var searchBtn = document.querySelector("#search-button");
+var userInput = document.querySelector("#user-search-input");
+var recentSearch = document.querySelector("#recent-search");
+
+
