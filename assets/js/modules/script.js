7f8a4930073b6057432caba3cafee3f240eb7fc7
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2022-12-18 14:50:26 +0000

    github codespaces experiment 2

--- assets/js/modules/script.js
+++ assets/js/modules/script.js
@@ -2,7 +2,7 @@
 
 // const YOUR_API_KEY = x;
 
-var API_KEY = process.env.YOUR_API_KEY; // register for a free key at openweathermap.com
+var API_KEY = YOUR_API_KEY; // register for a free key at openweathermap.com
 
 // Assign HTML Elements to variables
 var searchBtn = document.querySelector("#search-button");
